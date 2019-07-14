5aa55845053770d011bb3657803f8b253e0bf07c
tarou yamada <pamera.umaru124@gmail.com>
2019-07-14 06:25:43 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -11,7 +11,11 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>タスク</th>
+            <th>task</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+        <tr>
+            <th>status</th>
             <td>{{ $task->content }}</td>
         </tr>
     </table>
